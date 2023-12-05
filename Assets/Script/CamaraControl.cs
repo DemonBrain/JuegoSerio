40f7c584adfbba5f9b327d7caafc2b18e041bbda
DemonBrain <81323986+DemonBrain@users.noreply.github.com>
2023-12-05 03:14:34 -0300

    Tienda y camara, mod tornado

--- Assets/Script/CamaraControl.cs
+++ Assets/Script/CamaraControl.cs
@@ -2,12 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform player;
+    public List<Transform> targets;
 
-    private void Update()
+    public Vector3 offset;
+    public float smoothTime = .5f;
+
+    public float minZoom = 158f;
+    public float maxZoom = 120f;
+    public float zoomLimiter = 10f;
+
+    private Vector3 velocity;
+    private Camera cam;
+
+    private void Start()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        cam = GetComponent<Camera>();
+        offset = new Vector3(0, 0, -1);
+    }
+
+    void LateUpdate()
+    {
+
+        if (targets.Count == 0)
+            return;
+
+        Move();
+        Zoom();
+    }
+
+    void Zoom()
+    {
+        Debug.Log(GetGreatestDistance());
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+    void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; ++i)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+
+        for(int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.center;
     }
 }
